7499e18689cb12e20842ce33735f9156a4df4a6a
Alday <carlos.campos.alday@estudiantxs.uacm.edu.mx>
2025-10-04 19:49:35 -0600

    Definicones

--- practica1/funciones.c
+++ practica1/funciones.c
@@ -1,13 +1,29 @@
+#include <stdio.h>
 
-void swap (int *a, int *b) {
-  int temp = *a;
-  *a = *b;
-  *b = temp;
- }
+//Prototipos de funciones
+void swap (int *a, int *b);
 int longitudCadena ( char * cadena);
+  
 void invertirArreglo (int *arr , int size);
 int cuentaPares (int *arr , int size);
 
-void main(){
-  
+// Definición de swap
+void swap (int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Definición de longitudCadena
+int longitudCadena (char * cadena) {
+  int longitudCadena = 0;
+    while(*cadena != '\0') {
+        longitudCadena++;
+        cadena++;
+    }
+    return longitudCadena;
+}
+int main() {
+    // Puedes probar aquí tus funciones
+    return 0;
 }
