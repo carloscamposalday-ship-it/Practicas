2819d3708c5e8b5f9d7c80d04cfe44dcecb0cbeb
Alday <carlos.campos.alday@estudiantxs.uacm.edu.mx>
2025-10-04 18:20:33 -0600

    defi_punteros

--- practica1/funciones.c
+++ practica1/funciones.c
@@ -1,9 +1,13 @@
 
-void swap (int *a, int *b);
+void swap (int *a, int *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+ }
 int longitudCadena ( char * cadena);
 void invertirArreglo (int *arr , int size);
 int cuentaPares (int *arr , int size);
 
 void main(){
-
+  
 }
